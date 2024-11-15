266d413cb05c74b0a046270ac60301b3ccd9c5e3
Karen Weng <karen.wengxt@gmail.com>
2024-11-15 16:46:48 -0500

    added function TT

--- long/UpDownLeftRight.v
+++ long/UpDownLeftRight.v
@@ -350,3 +350,23 @@ module hex7seg (hex, display);
         endcase
 endmodule
 
+module shift_register_move_snake (clk, enable, maxLength, data, data_in, data_out);
+    input clk;
+    input enable;
+    input maxLength; 
+    input [ n * maxLength-1 :0 ] data;
+    input [ n-1 :0 ] data_in;
+    output reg [ n * maxLength-1 :0 ] data_out;
+
+    parameter n = 8;
+
+    always @(posedge clk) 
+    begin
+        if (enable) begin
+            // left is the head
+            // add new data to the front the rest follows
+            data_out <= {data_in, data[n * maxLength-1 : n-1 ]};
+        end
+    end
+
+endmodule
