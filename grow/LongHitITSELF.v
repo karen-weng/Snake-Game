e48973b60b7c3286b5d88c5374fe87a61dc742c3
Karen Weng <karen.wengxt@gmail.com>
2024-11-22 22:15:20 -0500

    Update LongHitITSELF.v

--- grow/LongHitITSELF.v
+++ grow/LongHitITSELF.v
@@ -6,7 +6,7 @@
 *   KEY[3] to start the animation. 
 */
 module vga_demo(CLOCK_50, SW, KEY, VGA_R, VGA_G, VGA_B,
-				VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK);
+				VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK, HEX);
 	
     parameter A = 4'b0000, B = 4'b0001, C = 4'b0010, D = 4'b0011; 
     parameter E = 4'b0100, F = 4'b0101, G = 4'b0110, H = 4'b0111; 
@@ -40,6 +40,7 @@ module vga_demo(CLOCK_50, SW, KEY, VGA_R, VGA_G, VGA_B,
     output VGA_BLANK_N;
     output VGA_SYNC_N;
     output VGA_CLK;	
+    output HEX[5:0];
 
     wire [7:0] VGA_X; 
     wire [6:0] VGA_Y;  
